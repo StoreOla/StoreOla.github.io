6645b5d5c2396d60b5332a26e565d631e7d93b41
Ola Glenna Grostølen <o-grost@online.no>
2023-06-21 15:02:28 +0200

    added html, css, js

---
+++ myscript.js
@@ -0,0 +1,80 @@
+function _f(id) {
+  return document.getElementById(id);
+}
+
+let div0 = _f('knuter0');
+let tgl0 = _f('toggle0');
+
+tgl0.onclick = function() {
+ if (div0.className == 'b') {
+   div0.className = 's';
+ } else {
+   div0.className = 'b'
+ }
+}
+
+let div1 = _f('knuter1');
+let tgl1 = _f('toggle1');
+
+tgl1.onclick = function() {
+ if (div1.className == 'b') {
+   div1.className = 's';
+ } else {
+   div1.className = 'b'
+ }
+}
+
+let div2 = _f('knuter2');
+let tgl2 = _f('toggle2');
+
+tgl2.onclick = function() {
+ if (div2.className == 'b') {
+   div2.className = 's';
+ } else {
+   div2.className = 'b'
+ }
+}
+
+let div3 = _f('knuter3');
+let tgl3 = _f('toggle3');
+
+tgl3.onclick = function() {
+ if (div3.className == 'b') {
+   div3.className = 's';
+ } else {
+   div3.className = 'b'
+ }
+}
+
+let div4 = _f('knuter4');
+let tgl4 = _f('toggle4');
+
+tgl4.onclick = function() {
+ if (div4.className == 'b') {
+   div4.className = 's';
+ } else {
+   div4.className = 'b'
+ }
+}
+
+let div5 = _f('knuter5');
+let tgl5 = _f('toggle5');
+
+tgl5.onclick = function() {
+ if (div5.className == 'b') {
+   div5.className = 's';
+ } else {
+   div5.className = 'b'
+ }
+}
+
+let div6 = _f('knuter6');
+let tgl6 = _f('toggle6');
+
+tgl6.onclick = function() {
+ if (div6.className == 'b') {
+   div6.className = 's';
+ } else {
+   div6.className = 'b'
+ }
+}
